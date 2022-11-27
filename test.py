3e483a6f2f2e6c5fababe48f6786393e9b619e9d
gilmiwired <meirumina@gmail.com>
2022-11-27 21:05:16 +0000

    "auto up"

--- test.py
+++ test.py
@@ -1,7 +1,9 @@
 import MeCab
 from matplotlib import pyplot as plt
 from wordcloud import WordCloud
-
+import numpy as np
+from PIL import Image
+#import cv2
 
 
 stop_word_list =['そう', 'ない', 'いる', 'する', 'まま', 'よう',
@@ -37,14 +39,45 @@ while node:
 # リストを文字列に変換
 word_chain = ' '.join(word_list)
 
+with open('test.txt', mode='rt', encoding='utf-8') as fi:
+    source_text = fi.read()
+
+# MeCabの準備
+tagger = MeCab.Tagger()
+tagger.parse('')
+node = tagger.parseToNode(source_text)
+
+# 名詞を取り出す
+word_list = []
+while node:
+    word_type = node.feature.split(',')[0]
+    if word_type == '名詞' or word_type =='形容詞':
+        word_list.append(node.surface)
+        #print(word_type + node.surface)
+    node = node.next
+
+# リストを文字列に変換
+word_chain = ' '.join(word_list)
 # ワードクラウド作成
 #W = WordCloud(width=640, height=480, background_color='white', colormap='bone', font_path='./JNRfont.ttf').generate(word_chain)
 
 #plt.imshow(W)
 #plt.axis('off')
 #plt.show()
+img = Image.open("./frogs.png")
+mask_img = np.array(img)
 
-wordcloud = WordCloud(background_color="white",font_path="./ipaexg.ttf",repeat=True,collocations=False,width=800,height=600)
+wordcloud = WordCloud(mask = mask_img,background_color="white",contour_width=5,font_path="./ipaexg.ttf",repeat=True,collocations=False,width=800,height=600)
 wordcloud.generate(word_chain)
 
 wordcloud.to_file("./word.png")
+
+#frame = cv2.imread("./word.png")
+#png_image = cv2.imread("./fros.png", cv2.IMREAD_UNCHANGED)  # アルファチャンネル込みで読み込む
+
+# 貼り付け先座標の設定。とりあえず左上に
+#x1, y1, x2, y2 = 0, 0, png_image.shape[1], png_image.shape[0]
+
+# 合成!
+#frame[y1:y2, x1:x2] = frame[y1:y2, x1:x2] * (1 - png_image[:, :, 3:] / 255) + \
+ #                     png_image[:, :, :3] * (png_image[:, :, 3:] / 255)
